c5c80b7181c7597c6b33e78d273ec5491a33fc52
danterusdev <danterusdev@gmail.com>
2023-10-29 17:12:48 -0600

    Hash Map implementation in core

--- build_with_core.sh
+++ build_with_core.sh
@@ -8,6 +8,6 @@ if [ "$1" = "valgrind" ]; then
     shift
 fi
 
-command="$command ${@} core/write.barely core/file.barely core/print.barely core/allocate.barely core/brk_allocator.barely core/linked_list.barely core/dynamic_array.barely core/assert.barely core/string.barely core/syscall.barely core/read.barely core/memory.barely core/string_parse.barely core/buffer.barely"
+command="$command ${@} core/write.barely core/file.barely core/print.barely core/allocate.barely core/brk_allocator.barely core/linked_list.barely core/dynamic_array.barely core/hash_map.barely core/assert.barely core/string.barely core/syscall.barely core/read.barely core/memory.barely core/string_parse.barely core/buffer.barely"
 command+=" core/barely/lexer.barely core/barely/parser.barely core/barely/ast.barely core/elf64.barely core/barely/backend/elf_linux_x64.barely core/x64.barely"
 eval "$command"
